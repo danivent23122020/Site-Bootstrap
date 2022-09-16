deb5776f231c6591858f274052c527a6844f9619
danivent23122020 <dani.vent@orange.fr>
2022-09-16 21:47:07 +0200

    modif

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
+// carousel Lightbox -> gallerie
 $(document).on("click", '[data-toggle="lightbox"]', function (event) {
     event.preventDefault();
     $(this).ekkoLightbox();
+});
+
+// carousel  ban2 -> txt
+$(".carousel").carousel({
+    interval: 2500,
+    pause: "null",
 });
