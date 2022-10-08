8a2f78f1bac08ec8fcdbf3ff85ed2a72dcc78787
danivent23122020 <dani.vent@orange.fr>
2022-10-08 18:27:45 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -21,3 +21,35 @@ window.addEventListener("scroll", function () {
     let scroll = this.document.querySelector(".navbar");
     scroll.classList.toggle("resize", window.scrollY > 30);
 });
+
+// *** cookie modal ***
+let cookieModal = document.querySelector("#cookieModal");
+let acceptCookie = document.querySelector("#acceptCookie");
+let cancelCookie = document.querySelector("#refusedCookie");
+//
+cancelCookie.addEventListener("click", function () {
+    cookieModal.classList.remove("active");
+});
+//
+acceptCookie.addEventListener("click", function () {
+    let d = new Date();
+    d.setMinutes(2 + d.getMinutes());
+    document.cookie = "AgencePrestaWeb42=true; expires = ' + d + ';";
+    cookieModal.classList.remove("active");
+});
+
+// check cookie exist
+const checkCookie = () => {
+    let input = document.cookie.split("=");
+    if (input[0] == "AgencePrestaWeb42") {
+        cookieModal.classList.remove("active");
+    } else {
+        cookieModal.classList.add("active");
+    }
+};
+// check cookie exist page already load
+window.onload = () => {
+    setTimeout(() => {
+        checkCookie();
+    }, 1000);
+};
